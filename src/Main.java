704f65b9ca56e1371b5688c1aceb6caf954eba11
BerntChristensen <redheadmail@gmail.com>
2013-04-19 21:57:28 +0200

    Ny uppdatering

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("Hej FrÃ¥n Elias");
+		System.out.println("Hej FrŒn Elias");
+		System.out.println("Hej Elias! //Bernt");
 	}
 }
