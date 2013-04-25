fbd98c90e57df500f79f7eff0d6fa0de01e7fedc
Stiss <g.lundsgard@gmail.com>
2013-04-25 17:36:45 +0200

    Fixat Serverpane, samt så kommer Olofs gamepane med. Ska joxxa i server så man faktiskt kan starta den

--- src/GUI/GamePane.java
+++ src/GUI/GamePane.java
@@ -1,17 +1,30 @@
 package GUI;
 
-import java.awt.BorderLayout;
-
+import java.awt.*;
 import javax.swing.*;
 
 public class GamePane extends JPanel {
-	
-	public GamePane(){
-		
-		JButton button = new JButton("GamePane");
-		add(button);
-		
-		
+	private JViewport pane;
+	public static JTextArea chatText = null;
+	public static JTextArea questionText = null;
+	public static JTextField chatLine = null;
+
+	public GamePane() {
+		JPanel chatPane = new JPanel(new BorderLayout());
+		chatText = new JTextArea(10, 20);
+		chatText.setLineWrap(true);
+		chatText.setEditable(false);
+		chatText.setForeground(Color.blue);
+		JScrollPane chatTextPane = new JScrollPane(chatText,
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		chatLine = new JTextField();
+		chatLine.setEnabled(true);
+		chatPane.add(chatLine, BorderLayout.SOUTH);
+		chatPane.add(chatTextPane, BorderLayout.CENTER);
+		chatPane.setPreferredSize(new Dimension(200, 200));
+		add(chatPane);
+
 	}
-	
+
 }
--- src/GUI/ServerPane.java
+++ src/GUI/ServerPane.java
@@ -3,69 +3,117 @@ package GUI;
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ServerPane extends JPanel {
+	private JLabel status;
+	private JTextField[] fields;
 
 	public ServerPane() {
 
+		fields = new JTextField[2];
 		GridBagLayout layout = new GridBagLayout();
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;		
-		setLayout(layout);
+		setLayout(layout);	
+		
 		JComponent comp;
+		
+		Font font = new Font("Quickz", Font.PLAIN,30);
+		JComponent top = new JLabel("Welcome to Quickz");
+		top.setFont(font);		
+		c.weightx = 1;
+		c.weighty=1;
+		c.gridwidth = 1000000;
+		c.gridx=2;
+		c.gridy=0;
+		c.ipadx=10;
+		
+		
+		add(top,c);
+		
+		
+		
 		comp= inputPane("Server name");
-		c.weightx = 40;
+		c.weightx = 20;
 		c.weighty=1;
 		c.gridx=1;
+		c.gridwidth = 2;
 		c.gridy=1;
 		add(comp,c);
 		
 		
 		comp= inputPane("Port number");
-		c.weightx = 40;
+		c.fill = GridBagConstraints.HORIZONTAL;		
+		c.weightx = 20;
 		c.weighty=1;
+		c.gridwidth = 2;
 		c.gridx=1;
 		c.gridy=2;
 		add(comp,c);
+		System.out.println(fields[0]);
+		
+		JButton button = new JButton("Create server");
+		c.fill = GridBagConstraints.HORIZONTAL;		
+		c.weightx=0;
+		c.weighty=0;
+		c.gridx = 2;
+		c.gridwidth = 1;
+		c.gridy = 3;
+		button.addActionListener(new ActionHandler());
+		add(button,c);
+		
+		status = new JLabel("Server not running");
+		c.fill = GridBagConstraints.HORIZONTAL;		
+		c.weightx=20;
+		c.weighty=1;
+		c.gridx = 1;
+		c.gridwidth = 1;
+		c.gridy = 4;
+		add(status,c);
+		
+		status = new JLabel("                            ");
+		c.fill = GridBagConstraints.HORIZONTAL;		
+		c.weightx=50;
+		c.weighty=1;
+		c.gridx = 3;
+		c.gridy = 4;
+		c.gridwidth = 1;
+		add(status,c);
 		
 		
-		comp = new JButton("NR2");
-		c.weightx=2;
-		c.weighty=10;
-		c.gridx = 100;
-		c.gridy = 100;
-		add(comp,c);
-		
-		JComponent input1 = inputPane("Port number");
-	
 				
-
-
-		
 	}
 
-	public void makeComponent(JComponent comp, GridBagLayout gridbag,
-			GridBagConstraints c) {		
-		
-		
 
-	}
-	
-	
 	public JComponent inputPane(String text){
-		JLabel label = new JLabel(text);
-		JTextField field = new JTextField(text,30);
-		field.setEditable(true);
-		field.setSize(10, 10);
-		
-		
-		JPanel p = new JPanel();
-		
+		int i =0;
+		JLabel label = new JLabel(text +"   ");
+		JTextField field = new JTextField(text,10);
+		field.setEditable(true);			
+		JPanel p = new JPanel();		
 		p.setLayout(new BorderLayout());
 		p.add(label,BorderLayout.WEST);
 		p.add(field,BorderLayout.CENTER);
-		
+		fields[i] = field;
+		i++;
 		return p;
+		
+	}
+	 
+	// Vad som händer när du trycker på knapps
+	class ActionHandler implements ActionListener{
+	int i =0;
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			i++;
+			System.out.println("Knapp-tryck HEHE");
+			System.out.println(fields[0].getText());
+			
+			
+		}
+		
 		
 	}
 
