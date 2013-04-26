152876571ad2d36d5aff49da8f4ce3ddf165c055
Stiss <g.lundsgard@gmail.com>
2013-04-26 10:02:30 +0200

    Nu försvinner texten när man trycker på den.

--- src/GUI/ServerPane.java
+++ src/GUI/ServerPane.java
@@ -7,128 +7,137 @@ import Server.Server;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ServerPane extends JPanel {
 	private JLabel status;
 	private JTextField[] fields;
 	private JLabel emptyLabel;
+	private int i;
 
-	public ServerPane() {
+	public ServerPane() { 
+		i=0;
 
 		fields = new JTextField[2];
 		GridBagLayout layout = new GridBagLayout();
 		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.HORIZONTAL;		
-		setLayout(layout);	
-		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		setLayout(layout);
+
 		JComponent comp;
-		
-		Font font = new Font("Quickz", Font.PLAIN,30);
+
+		Font font = new Font("Quickz", Font.PLAIN, 30);
 		JComponent top = new JLabel("Welcome to Quickz");
-		top.setFont(font);		
+		top.setFont(font);
 		c.weightx = 1;
-		c.weighty=1;
+		c.weighty = 1;
 		c.gridwidth = 1000000;
-		c.gridx=2;
-		c.gridy=0;
-		c.ipadx=10;
-		
-		
-		add(top,c);
-		
-		
-		
-		comp= inputPane("Server name");
+		c.gridx = 2;
+		c.gridy = 0;
+		c.ipadx = 10;
+
+		add(top, c);
+
+		comp = inputPane("Server name");
 		c.weightx = 20;
-		c.weighty=1;
-		c.gridx=1;
+		c.weighty = 1;
+		c.gridx = 1;
 		c.gridwidth = 2;
-		c.gridy=1;
-		add(comp,c);
-		
-		
-		comp= inputPane("Port number");
-		c.fill = GridBagConstraints.HORIZONTAL;		
+		c.gridy = 1;
+		add(comp, c);
+
+		comp = inputPane("Port number");
+		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 20;
-		c.weighty=1;
+		c.weighty = 1;
 		c.gridwidth = 2;
-		c.gridx=1;
-		c.gridy=2;
-		add(comp,c);
+		c.gridx = 1;
+		c.gridy = 2;
+		add(comp, c);
 		System.out.println(fields[0]);
-		
+
 		JButton button = new JButton("Create server");
-		c.fill = GridBagConstraints.HORIZONTAL;		
-		c.weightx=0;
-		c.weighty=0;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0;
+		c.weighty = 0;
 		c.gridx = 2;
 		c.gridwidth = 1;
 		c.gridy = 3;
 		button.addActionListener(new ActionHandler());
-		add(button,c);
-		
+		add(button, c);
+
 		status = new JLabel("Server not running");
-		c.fill = GridBagConstraints.HORIZONTAL;		
-		c.weightx=20;
-		c.weighty=1;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 20;
+		c.weighty = 1;
 		c.gridx = 1;
 		c.gridwidth = 1;
 		c.gridy = 4;
-		add(status,c);
-		
-		emptyLabel= new JLabel("                            ");
-		c.fill = GridBagConstraints.HORIZONTAL;		
-		c.weightx=50;
-		c.weighty=1;
+		add(status, c);
+
+		emptyLabel = new JLabel("                            ");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 50;
+		c.weighty = 1;
 		c.gridx = 3;
 		c.gridy = 4;
 		c.gridwidth = 1;
-		add(emptyLabel,c);
+		add(emptyLabel, c);
 		
+		fields[1].addMouseListener(new MouseAdapter() {
+			public void mouseReleased(MouseEvent e) {
+				fields[1].setText("");
+			}
+		});
+		fields[0].addMouseListener(new MouseAdapter() {
+			public void mouseReleased(MouseEvent e) {
+				fields[0].setText("");
+			}
+		});
 		
-				
+
 	}
 
-
-	public JComponent inputPane(String text){
-		int i =0;
-		JLabel label = new JLabel(text +"   ");
-		JTextField field = new JTextField(text,10);
-		field.setEditable(true);			
-		JPanel p = new JPanel();		
+	public JComponent inputPane(String text) {
+		
+		JLabel label = new JLabel(text + "   ");
+		JTextField field = new JTextField(text, 10);
+		field.setEditable(true);
+		JPanel p = new JPanel();
+	
 		p.setLayout(new BorderLayout());
-		p.add(label,BorderLayout.WEST);
-		p.add(field,BorderLayout.CENTER);
+		p.add(label, BorderLayout.WEST);
+		p.add(field, BorderLayout.CENTER);
 		fields[i] = field;
 		i++;
 		return p;
-		
+
 	}
-	 
-	// Vad som händer när du trycker på knapps
-	class ActionHandler implements ActionListener{
 	
+
+
+	// Vad som händer när du trycker på knapps
+	class ActionHandler implements ActionListener {
+
 		@Override
 		public void actionPerformed(ActionEvent event) {
-			
-			
-			String test =fields[0].getText();
-			
-			try{
-			int port = Integer.parseInt(test);	
-			Server serv = new Server(port);
-			serv.start();
-			status.setText("Server running");
-			emptyLabel.setText("                ");
-			}catch(Exception e){
+
+			String test = fields[0].getText();
+
+			try {
+				int port = Integer.parseInt(test);
+				Server serv = new Server(port);
+				serv.start();
+				status.setText("Server running");
+				emptyLabel.setText("                ");
+			} catch (Exception e) {
 				emptyLabel.setText("Felaktigt portnummer");
-				
+
 			}
-			
-			
+
 		}
-		
-		
+
 	}
 
 }
